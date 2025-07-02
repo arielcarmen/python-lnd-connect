f8bf34be4c801f06af8e90a0b827ad17ab600afd
Ariel Carmen <arielahognisse@gmail.com>
2025-07-02 11:32:55 +0100

    fix : railway : .env

--- setup2.sh
+++ setup2.sh
@@ -1,4 +1,4 @@
+cp .env.example .env
 pip install -r requirements.txt 
 curl -o lightning.proto https://raw.githubusercontent.com/lightningnetwork/lnd/master/lnrpc/lightning.proto 
 python -m grpc_tools.protoc --proto_path=. --python_out=. --grpc_python_out=. lightning.proto; 
-cp .env .env.example
