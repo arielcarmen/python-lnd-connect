7980dfd7bbc120b0ae8450f395df495c660fc172
Ariel Carmen <arielahognisse@gmail.com>
2025-07-02 23:09:56 +0100

    er

--- jsoner.py
+++ jsoner.py
@@ -5,7 +5,7 @@ b64 = os.getenv("GOOGLE_SERVICE_ACCOUNT_B64")
 
 if b64:
     decoded = base64.b64decode(b64)
-    with open("serviceAccount.json", "wb") as f:
+    with open("med-book.json", "wb") as f:
         f.write(decoded)
     print("✅ serviceAccount.json généré avec succès")
 else:
